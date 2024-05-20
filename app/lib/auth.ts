5f8a8d42cdec84f0e9355ec4f77611c5a13142a8
Yoshi K <kai.outsider.777+git@gmail.com>
2024-05-21 06:23:52 +0900

    cli 認証時の検証を強化

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -15,7 +15,9 @@ export function generateAccessToken() {
 
 export function isInvalidClientToken(token: string) {
   try {
-    return decryptTokenToDateTime(token)
+    const dt = decryptTokenToDateTime(token);
+    if (!dt.isValid()) return true;
+    return dt
       .add(Number(process.env.ACCESS_TOKEN_EXPIRATION_MONTH), "M")
       .add(Number(process.env.ACCESS_TOKEN_EXPIRATION_DAY_OF_MONTH), "d")
       .isBefore(dayjs());
