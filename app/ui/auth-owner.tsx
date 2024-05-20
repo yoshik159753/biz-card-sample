f84b22774c616be50b1c6349d02c99a65355eafb
Yoshi K <kai.outsider.777+git@gmail.com>
2024-05-21 06:41:18 +0900

    own 認証時の背景色をクリア

--- app/ui/auth-owner.tsx
+++ app/ui/auth-owner.tsx
@@ -24,22 +24,21 @@ export default function AuthOwner() {
 
   return (
     <>
-      <div className="relative h-screen bg-red-200/80">
+      <div className="relative h-screen">
         <p>Not Found...</p>
-        <div className="fixed top-0 left-0 right-0 h-screen grid gap-1 grid-cols-3 grid-rows-4 bg-blue-200/50">
-          {/* TODO: 最終的には色を消す */}
-          <div className="bg-green-200/50" onClick={() => onClickNum("1")} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("2")} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("3")} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("4")} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("5")} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("6")} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("7")} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("8")} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("9")} />
-          <div className="bg-green-200/50" onClick={() => onClickReset()} />
-          <div className="bg-green-200/50" onClick={() => onClickNum("0")} />
-          <div className="bg-green-200/50" onClick={() => onClickReset()} />
+        <div className="fixed top-0 left-0 right-0 h-screen grid gap-1 grid-cols-3 grid-rows-4">
+          <div onClick={() => onClickNum("1")} />
+          <div onClick={() => onClickNum("2")} />
+          <div onClick={() => onClickNum("3")} />
+          <div onClick={() => onClickNum("4")} />
+          <div onClick={() => onClickNum("5")} />
+          <div onClick={() => onClickNum("6")} />
+          <div onClick={() => onClickNum("7")} />
+          <div onClick={() => onClickNum("8")} />
+          <div onClick={() => onClickNum("9")} />
+          <div onClick={() => onClickReset()} />
+          <div onClick={() => onClickNum("0")} />
+          <div onClick={() => onClickReset()} />
         </div>
       </div>
     </>
