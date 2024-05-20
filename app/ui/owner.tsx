9f273c2fc90b403a08e20c7b58d80b3cd16a8dd0
Yoshi K <kai.outsider.777+git@gmail.com>
2024-05-21 05:29:59 +0900

    owner page に qrcode を表示

--- app/ui/owner.tsx
+++ app/ui/owner.tsx
@@ -1,12 +1,16 @@
 import { generateAccessToken } from "@/app/lib/auth";
+import Image from "next/image";
+import QRCode from "qrcode";
 
-export default function Owner() {
+export default async function Owner() {
   const token = generateAccessToken();
-
+  const url = `${process.env.BASE_URL}/?token=${token}`;
+  const qrcodeImg = await QRCode.toDataURL(url);
   return (
     <>
       <h1>Owner page</h1>
       <p>token: {token}</p>
+      <Image src={qrcodeImg} width={150} height={150} alt="qrcode" />
     </>
   );
 }
